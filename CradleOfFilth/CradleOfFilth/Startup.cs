f2e74ac3020738e3d83073cca39e6c92ece57ede
jcqnly <jcqnly@gmail.com>
2018-07-15 12:15:04 -0700

    add Bootstrap CDN link and a landing page

--- CradleOfFilth/CradleOfFilth/Startup.cs
+++ CradleOfFilth/CradleOfFilth/Startup.cs
@@ -15,6 +15,7 @@ namespace CradleOfFilth
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -24,10 +25,18 @@ namespace CradleOfFilth
             {
                 app.UseDeveloperExceptionPage();
             }
+            app.UseStaticFiles();
+
+            app.UseMvc(route =>
+            {
+                route.MapRoute(
+                    name: "default",
+                    template: "{controller=Home}/{action=Index}/{id?}");
+            });
 
             app.Run(async (context) =>
             {
-                await context.Response.WriteAsync("Hello World!");
+                await context.Response.WriteAsync("Got kicked out for not being metal enough!");
             });
         }
     }
