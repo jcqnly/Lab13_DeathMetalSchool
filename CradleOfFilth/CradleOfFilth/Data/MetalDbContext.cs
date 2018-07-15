37902d69c750465f6c0d29c7c82cd96fd7718912
jcqnly <jcqnly@gmail.com>
2018-07-15 12:37:19 -0700

    add DbConnection and Dependency Injection

---
+++ CradleOfFilth/CradleOfFilth/Data/MetalDbContext.cs
@@ -0,0 +1,15 @@
+﻿using CradleOfFilth.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace CradleOfFilth.Data
+{
+    public class MetalDbContext : DbContext
+    {
+        public MetalDbContext(DbContextOptions<MetalDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Course> Courses { get; set; }
+        public DbSet<Student> Students { get; set; }
+    }
+}
