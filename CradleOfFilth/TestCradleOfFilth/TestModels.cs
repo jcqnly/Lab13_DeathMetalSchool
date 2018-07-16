51aaf35c60d147c1e1196c1b835dd134a7ed5f7a
jcqnly <jcqnly@gmail.com>
2018-07-15 23:15:56 -0700

    add search functionality

---
+++ CradleOfFilth/TestCradleOfFilth/TestModels.cs
@@ -0,0 +1,18 @@
+﻿using CradleOfFilth.Models;
+using System;
+using Xunit;
+
+namespace TestCradleOfFilth
+{
+    public class TestModels
+    {
+        [Fact]
+        public void CourseNameGetterTest()
+        {
+            Course course = new Course();
+            course.Name = "Cheez Wiz";
+
+            Assert.Equal("Cheez Wiz", course.Name);
+        }
+    }
+}
